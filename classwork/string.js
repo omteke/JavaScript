dc0ce238dac3e54e9240788ecf6e284a1743c19c
omteke <omteke2005@gmail.com>
2026-01-19 17:12:37 +0530

    classwork

---
+++ classwork/string.js
@@ -0,0 +1,33 @@
+// this is good to print string
+// String Interpolation
+let name = "om"
+console.log(`hello my name is ${name}`);
+
+const a = new String("om")
+console.log(a)
+console.log(a.length)
+console.log(a[0])
+console.log(a[1])
+console.log(a.charAt(1))
+console.log(a.toUpperCase(1))
+console.log(a.toLowerCase(2))
+console.log(a.toLowerCase(2))
+console.log(a.toLowerCase(2))
+console.log(a.toLowerCase(2))
+console.log(a.toLowerCase(2))
+console.log(a.toLowerCase(2))
+console.log(a.toLowerCase(2))
+
+
+
+// console.log(a.Charat[0])
+let b = "  om  "
+console.log(b)
+console.log(b.trim())
+
+
+
+
+
+
+
